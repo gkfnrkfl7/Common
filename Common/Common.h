5f873a27fc6615a8e33175093850379009321dd7
Peter <gkfnrkfl@naver.com>
2020-01-28 14:39:36 +0900

    Initial Commit

---
+++ Common/Common.h
@@ -0,0 +1,19 @@
+//
+//  Common.h
+//  Common
+//
+//  Created by 박기석 on 2020/01/28.
+//  Copyright © 2020 Peter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Common.
+FOUNDATION_EXPORT double CommonVersionNumber;
+
+//! Project version string for Common.
+FOUNDATION_EXPORT const unsigned char CommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Common/PublicHeader.h>
+
+
