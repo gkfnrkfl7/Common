097fa451cc8fe2cffd75e847d11e141589597ab6
Peter <gkfnrkfl@naver.com>
2020-02-01 14:01:19 +0900

    Hex init add

--- Common/UIColor.swift
+++ Common/UIColor.swift
@@ -11,7 +11,30 @@ import UIKit
 extension UIColor {
     
     // MARK: rgb 255 init
-    convenience init(r: Int, g: Int, b: Int, a: CGFloat? = nil) {
-        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a ?? 255) / 255)
+    
+    convenience init(r: Int, g: Int, b: Int, alpha: CGFloat? = nil) {
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(alpha ?? 255) / 255)
+    }
+    
+    // MARK: hex 8 (alpha+rgb) init
+    
+    convenience init(hex8 color: Int) {
+        let mask = 0x000000FF
+        
+        let alpha = CGFloat(color >> 24 & mask) / 255.0
+        
+        self.init(hex6: color, alpha: alpha)
+    }
+    
+    // MARK: hex 6 init
+    
+    convenience init(hex6 color: Int, alpha: CGFloat? = nil) {
+        let mask = 0x000000FF
+        
+        let red = CGFloat(color >> 16 & mask) / 255.0
+        let green = CGFloat(color >> 8 & mask) / 255.0
+        let blue = CGFloat(color & mask) / 255.0
+        
+        self.init(red: red, green: green, blue: blue, alpha: alpha ?? 255)
     }
 }
